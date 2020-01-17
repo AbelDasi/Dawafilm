feb3ee8c145dc0950ead68090b398e157864fb72
Daniel Serrano <danielsercar@mdolivar.com>
2020-01-17 09:40:09 +0100

    Dawafilms que funciona

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,66 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  constructor(private router: Router) {}
+  forgotString : string = "Forgot your password?";
+  name: string = "";
+  passwd: string = "";
+  labelMsg: string = "";
+  validator: boolean  = true;
 
-  register(){
-    this.router.navigateByUrl("register");
+
+constructor(private router:Router, public alertController: AlertController) {}
+async handleButtonClick() {
+  const alert = await this.alertController.create({
+    header: 'Use this lightsaber?',
+    message: 'Do you agree to use this lightsaber to do good across the galaxy?',
+    inputs: [
+      {
+        name: 'email',
+        type: 'text',
+        placeholder: 'Please put ur e-mail here...'
+      }],
+    buttons: [{
+      text: 'Cancel',
+      role: 'cancel',
+      cssClass: 'secondary',
+      handler: () => {
+        console.log('Confirm Cancel');
+      }
+    }, {
+      text: 'Ok',
+      handler: data => {
+        console.log(data.email);
+      }
+    }]
+  }).then(alert => {
+    alert.present();
+  });
+}
+
+changeButtonString(){
+  this.forgotString="Set your password";
+}
+validateInputs(){
+  if(this.name.trim() === "" || this.passwd.trim() === ""){
+    this.labelMsg = "You cannot make that!";
+    this.validator = false;
+  }else{
+    this.router.navigateByUrl("tabs");
+    this.labelMsg = "";
   }
+}
+validateInputs2(){
+  if(this.validator === false){
+    if(this.name.trim() === "" || this.passwd.trim() === ""){
+      this.labelMsg = "You cannot make that!";
+      this.validator = false;
+    }else{
+      this.labelMsg = "";
+    }
+  }
+}
+resgistry(){
+  this.router.navigateByUrl("register");
+}
+
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  forgotString : string = "Forgot your password?";
+  name: string = "";
+  passwd: string = "";
+  labelMsg: string = "";
+  validator: boolean  = true;
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
   }
 
+  changeButtonString(){
+    this.forgotString="Set your password";
+  }
+  validateInputs(){
+    if(this.name.trim() === "" || this.passwd.trim() === ""){
+      this.labelMsg = "You cannot make that!";
+      this.validator = false;
+    }else{
+      this.name = "";
+      this.passwd = "";
+      this.router.navigateByUrl("home");
+      this.labelMsg = "";
+    }
+  }
+  validateInputs2(){
+    if(this.validator === false){
+      if(this.name.trim() === "" || this.passwd.trim() === ""){
+        this.labelMsg = "You cannot make that!";
+        this.validator = false;
+      }else{
+        this.name = "";
+        this.passwd = "";
+        this.router.navigateByUrl("home");
+        this.labelMsg = "";
+      }
+    }
+  }
 }
